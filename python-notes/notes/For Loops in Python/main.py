432fb24bd3c18894ca63b349624309b8a4300ea4
Ankur <ankurdhar23@gmail.com>
2025-08-08 00:49:12 +0530

    Added 1 new file

---
+++ python-notes/notes/For Loops in Python/main.py
@@ -0,0 +1,22 @@
+# name = "Abhishek"
+# for i in name:
+#     print(i)
+#     if (i == 'h'):
+#         print("This is special!")
+# colors = ["Red", "Green", "Blue", "Yellow"]
+# for color in colors:
+#     print(color)    
+#     for i in color:
+#         print(i)
+# for k in range(1, 21):
+#     print(k)   
+# for k in range(21):
+#     print(k)          
+# for k in range(20):
+#     print(k + 1)            
+# for k in range(201):
+#     print(k)  
+for k in range(1, 16):
+    print(k)         
+for k in range(1, 17, 3):
+    print(k)
