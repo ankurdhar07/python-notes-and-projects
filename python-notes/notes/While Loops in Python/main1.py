fc891a8918f89586dc31cdd91d66158d43e38f8c
Ankur <ankurdhar23@gmail.com>
2025-08-09 01:56:49 +0530

    New note added

---
+++ python-notes/notes/While Loops in Python/main1.py
@@ -0,0 +1,33 @@
+#Python while Loop
+# i = 10
+# while i > 0:
+#     print(i)
+#     i = i - 1
+# h = 0
+# while h <= 10:
+#     print(h)
+#     h = h + 1
+# f = 1
+# while f <= 22:
+#     print(f)
+#     f = f + 1
+# f = 1
+# while f <= 23:
+#     print(f)
+#     f = f + 2   
+# f = 1
+# while f < 23:
+#     print(f)
+#     f = f + 2      
+# x = int(input("Enter your value of number :"))
+# while (x<=50):   
+#     x = int(input("Enter your value of number :")) 
+#     print(x)
+
+# print("Done with the value")
+count = 5
+while ( count > 0):
+    print(count)
+    count = count - 1
+else:
+    print("I am inside the else")
---
+++ python-notes/notes/While Loops in Python/main2.py
@@ -0,0 +1,33 @@
+# f = 30
+# while (f > 0):
+#     print(f)
+#     f = f - 1
+# g = 0
+# while (g <= 20):
+#     print(g)
+#     g = g + 1
+# a = int(input("Enter your value:"))
+# while (a <= 50):
+#     a = int(input("Enter your value :"))
+#     print(a)
+# else:
+#     print("i am in else")
+
+x = int(input("Enter your value :"))    
+while (x <= 0):
+    x = int(input("Enter your value :"))
+    print(x)
+else:
+    print("I am inside else!")
+
+# #do while loop:
+
+# while True:
+#     a = int(input("Enter a nagative number :"))
+#     print(a)
+#     if not (a < 0):
+#         break
+# count = 1
+# while (count < 21):
+#     print(count)
+#     count = count + 1
